4bbc3ba22b63722a9c8525a44c3f7f56b063853b
Daryl Pinto <daryl.j.pinto@gmail.com>
2021-04-11 19:52:28 -0400

    refactor: improve performance per tick

--- src/lib/world.rs
+++ src/lib/world.rs
@@ -28,40 +28,52 @@ impl World {
         }
     }
 
-    // Any live cell with two or three live neighbours survives.
-    // Any dead cell with three live neighbours becomes a live cell.
-    // All other live cells die in the next generation. Similarly, all other dead cells stay dead.
     pub fn tick(&mut self) {
+        // First pass, update neighbor count for every cell
         for i in 0..self.grid.len() {
+            // If the cell is dead and has no neighors,
+            // don't bother doing anything with it
+            if self.grid[i].state == 0 {
+                continue;
+            }
+
+            // If the cell is alve, visit each neighbor and increment their
+            // internal "living neighbor" state
             let is_alive = self.grid[i].is_alive();
 
-            // Update neighbor count for each cell
-            if self.grid[i].state > 0 {
+            if is_alive {
                 let neighbor_indicies = utils::get_neighbor_indicies(i, GRID_WIDTH, GRID_HEIGHT);
 
-                if is_alive {
-                    for neighbor_index in neighbor_indicies.list.iter() {
-                        match *neighbor_index {
-                            Some(index) => self.grid[index].increment_living_neighbor_count(),
-                            None => (),
-                        }
+                for neighbor_index in neighbor_indicies.list.iter() {
+                    match *neighbor_index {
+                        Some(index) => self.grid[index].increment_living_neighbor_count(),
+                        None => (),
                     }
                 }
             }
         }
 
+        // Second pass, set living/dead state
         for i in 0..self.grid.len() {
+            // If the cell is dead and has no neighors,
+            // don't bother doing anything with it
+            if self.grid[i].state == 0 {
+                continue;
+            }
+
+            // Any live cell with two or three live neighbours survives.
+            // Any dead cell with three live neighbours becomes a live cell.
+            // All other live cells die in the next generation. Similarly, all other dead cells stay dead.
             let is_alive = self.grid[i].is_alive();
             let living_neighbors = self.grid[i].get_living_neighbor_count();
 
-            // Update living state based on neighbors
             if is_alive && (living_neighbors < 2 || living_neighbors > 3) {
                 self.grid[i].die();
             } else if !is_alive && living_neighbors == 3 {
                 self.grid[i].spawn();
             }
 
-            // Set the neighbor count back to 0 for each cell
+            // Set the living neighbor count back to 0
             self.grid[i].reset_neighbor_count();
         }
     }
