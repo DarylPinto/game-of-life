de25fd6f8e25e8b91014d5a660f00fe1116e46d0
Daryl Pinto <daryl.j.pinto@gmail.com>
2021-04-12 15:45:28 -0400

    refactor: use matrix for world grid

--- src/lib/neighbor_list_index.rs
+++ src/lib/neighbor_list_index.rs
@@ -1,14 +1,14 @@
 #[derive(Debug)]
-pub struct NeighborIndexList {
-    pub list: [Option<usize>; 8],
+pub struct NeighborPositionList {
+    pub list: [Option<(usize, usize)>; 8],
 }
 
-impl NeighborIndexList {
+impl NeighborPositionList {
     pub fn new() -> Self {
         Self { list: [None; 8] }
     }
 
-    pub fn insert(&mut self, to_insert: usize) {
+    pub fn insert(&mut self, to_insert: (usize, usize)) {
         for (i, item) in self.list.iter_mut().enumerate() {
             match item {
                 None => {
@@ -18,18 +18,18 @@ impl NeighborIndexList {
                 Some(_) => (),
             }
         }
-        panic!("Cannot insert more than 8 values into NeighborIndexList");
+        panic!("Cannot insert more than 8 values into NeighborPositionList");
     }
 
     // WARNING: This should ONLY be used for testing, as it's rather expensive to perform multiple times per frame
     // Collapse the sorted neighbor list into a sorted vector
     #[cfg(test)]
-    pub fn debug_collapse(&self) -> Vec<usize> {
+    pub fn debug_collapse(&self) -> Vec<(usize, usize)> {
         let mut collapsed = self
             .list
             .iter()
             .filter_map(|item| *item)
-            .collect::<Vec<usize>>();
+            .collect::<Vec<(usize, usize)>>();
         collapsed.sort();
         collapsed
     }
